77aa1516881bc0e25c37a70d7bce9f0e58775b6a
VimalanathanThanushan <thanushaan69@gmail.com>
2025-12-12 17:09:43 +0530

    FEAT: Completed home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
+import Footer from './components/Footer'
 const App = () => {
 
   const isOwnerPath = useLocation().pathname.includes("owner")
@@ -10,11 +11,13 @@ const App = () => {
     <div>
       {!isOwnerPath && <Navbar/>}
       <div className='min-h-[70vh]'>
+
         <Routes>
           <Route path='/' element={<Home/>} />
         </Routes>
 
       </div>
+      <Footer />
     </div>
   )
 }
