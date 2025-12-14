00769647c97b328dc7fd0e971b06cf2e692e4d41
VimalanathanThanushan <thanushaan69@gmail.com>
2025-12-14 16:12:52 +0530

    Feat:completed booking system

--- client/src/pages/RoomDetails.jsx
+++ client/src/pages/RoomDetails.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import {useParams} from 'react-router-dom'
-import {roomsDummyData} from '../assets/assets'
+import {assets, facilityIcons, roomsDummyData} from '../assets/assets'
+import StarRating from '../components/StarRating'
 
 const RoomDetails = () => {
 
@@ -26,6 +27,101 @@ const RoomDetails = () => {
             20% Off
         </p>
       </div>
+      {/**Room Rating */}
+      <div className='flex items-center gap-1 mt-2'>
+        <StarRating/>
+        <p className='ml-2'>200+ reviews</p>
+      </div>
+
+      {/**Room Address */}
+      <div className='flex items-center gap-1 text-1 text-gray-500 mt-2'>
+        <img  src={assets.locationIcon} alt="location-icon" />
+        <span>{room.hotel.address}</span>
+      </div>
+
+      {/* Room Images */}
+      <div className='flex flex-col lg:flex-row mt-6 gap-6'>
+        <div className='lg:w-1/2 w-full'>
+           <img src={mainImage} alt="Room Image"
+           className='w-full rounded-xl shadow-lg object-cover' />
+        </div>
+        <div className='grid grid-cols-2 gap-4 lg:w-1/2 w-full'>
+          {room ?.images.length > 1 && room.images.map((image,index)=>(
+            <img onClick={()=> setMainImage(image)}
+             className={`w-full rounded-xl shadow-md object-cover cursor-pointer 
+              ${mainImage === image && 'outline-3 outline-orange-500'}`} 
+              key={index} src={image} alt="Room Image" />
+          ))}
+        </div>
+      </div>
+
+      {/* Room Highlights */}
+      <div className='flex flex-col md:flex-row md:justify-between mt-10'>
+            <div className='flex flex-col'>
+
+              <h1 className='text-3xl md:text-4xl font-playfair'>Experience Luxury Like Never Before</h1>
+
+              <div className='flex flex-wrap items-center mt-3 mb-6 gap-4'>
+
+                {room.amenities.map((item,index)=>(
+                  <div key={index} className='flex items-center gap-2 px-3 py-2 rounded-lg bg-gray-100'>
+                    <img className='w-5 h-5' src={facilityIcons[item]} alt={item} />
+                    <p className='text-xs'>{item}</p>
+                  </div>
+                ))}
+
+              </div>
+
+            </div>
+            {/*Room Price */}
+              <p className='text-2xl font-medium'>${room.pricePerNight}/night</p>
+
+      </div>
+      {/* CheckIn CheckOut Form  */}
+      <form className='flex flex-col md:flex-row items-start md:items-center justify-between
+                        bg-white shadow-[0px_0px_20px_rgba(0,0,0,0.15)] 
+                        p-6 rounded-xl mx-auto mt-16 max-w-6xl'>
+
+          <div className='flex flex-col flex-wrap md:flex-row 
+               items-start md:items-center gap-4 md:gap-10 text-gray-500'>
+
+             <div className='flex flex-col'>
+
+                        <label htmlFor="checkInDate" className='font-medium'>Check-In</label>
+                        <input className='w-full rounded border border-gray-300 px-3
+                               py-2 mt-1.5 outline-none' type="date"  id='checkInDate' placeholder='check-In' required />
+
+              </div>
+              <div className='w-px h-15 bg-gray-300/70 max-md:hidden'></div>
+              <div className='flex flex-col'>
+
+                        <label htmlFor="checkOutDate" className='font-medium'>Check-Out</label>
+                        <input className='w-full rounded border border-gray-300 px-3
+                               py-2 mt-1.5 outline-none' type="date"  id='checOutDate' 
+                               placeholder='Check-Out' required />
+
+              </div>
+              <div className='w-px h-15 bg-gray-300/70 max-md:hidden'></div>
+              <div className='flex flex-col'>
+
+                        <label htmlFor="guests" className='font-medium'>Guests</label>
+                        <input  type="number"  id='guests' placeholder='0' 
+                        className='max-w-20 rounded border border-gray-300 px-3 py-2 mt-1.5 outline-none' required />
+
+              </div>
+
+
+          </div>                  
+               
+
+          <button type='submit' className='bg-primary hover:bg-primary-dull 
+                                active:scale-95 transition-all text-white rounded-md 
+                                max-md:w-full max-md:mt-6 md:px-25 py-3 md:py-4 text-base cursor-pointer'>
+              Book Now
+          </button>
+
+      </form>
+      
     </div>
   )
 }
